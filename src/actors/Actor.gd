34cd60b3ab7f41537c1d1edc2d7cd119e75d7678
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-02 19:42:45 +0000

    Players's Movement Logic Implemented

--- src/actors/Actor.gd
+++ src/actors/Actor.gd
@@ -7,9 +7,3 @@ export var gravity := 3000.0
 
 var velocity := Vector2.ZERO
 
-
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	velocity.y = min(speed.y, velocity.y)
-	velocity = move_and_slide(velocity)
-	
