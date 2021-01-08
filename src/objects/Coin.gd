ddd1329c0ccaf2ec00f1b96e5fdcbc2204d0010c
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-08 18:19:17 +0000

    Coin - Added the logic to Increase the Player Score

--- src/objects/Coin.gd
+++ src/objects/Coin.gd
@@ -4,5 +4,9 @@ extends Area2D
 onready var animationPlayer : AnimationPlayer = $AnimationPlayer
 
 
+export var score : int = 100
+
+
 func _on_body_entered(body: Node) -> void:
+	PlayerData.score += score
 	animationPlayer.play("fade_out")
