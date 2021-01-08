7f0075c6d651a5cba277faf772e62268b1c76750
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-08 18:17:27 +0000

    Enemy - Refactor the Enemy to have all the Die logic on its own method and Added the logic to increase the Player Score on the PlayerData Autoload

--- src/actors/Enemy.gd
+++ src/actors/Enemy.gd
@@ -1,16 +1,20 @@
 extends "res://src/actors/Actor.gd"
 
 
+onready var collisionShape : CollisionShape2D = $CollisionShape2D
+onready var stompDetector : Area2D = $StompDetector
+
+export var score : int = 100
+
 func _ready() -> void:
 	_velocity.x = -speed.x
 
 
 func _on_StompDetector_body_entered(body: Node) -> void:
-	if body.global_position.y > $StompDetector.global_position.y:
+	if body.global_position.y > stompDetector.global_position.y:
 		return
 	
-	$CollisionShape2D.disabled = true
-	queue_free()
+	die()
 
 
 func _physics_process(delta: float) -> void:
@@ -22,3 +26,9 @@ func _physics_process(delta: float) -> void:
 	var snap := Vector2.DOWN * 65
 	_velocity.y = move_and_slide_with_snap(_velocity, snap, Vector2.UP).y
 	
+
+
+func die() -> void:
+	collisionShape.disabled = true
+	queue_free()
+	PlayerData.score += 100
