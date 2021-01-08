a33c3445b777bf91190c58a951234871d30ce65d
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-08 18:12:41 +0000

    Player - Refactor the Die Logic to its own method and add a call to the PlayerData Autoload to increase the number os deaths

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -9,7 +9,7 @@ func _on_StompDetector_area_entered(area: Area2D) -> void:
 
 
 func _on_EnemyDetector_body_entered(body: Node) -> void:
-	queue_free()
+	die()
 
 
 func _physics_process(delta: float) -> void:
@@ -49,4 +49,9 @@ func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vecto
 	var out := linear_velocity
 	out.y = -impulse
 	return out
+
+
+func die() -> void:
+	PlayerData.deaths += 1
+	queue_free()
 
