0dc5eb7d96b50dccaf494e73fd1e38dd101a28e3
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-09 18:39:48 +0000

    PlayerData - Added a reset function that resets the Score and the Deaths count

--- src/autoload/PlayerData.gd
+++ src/autoload/PlayerData.gd
@@ -9,6 +9,11 @@ var score := 0 setget set_score
 var deaths := 0 setget set_deaths
 
 
+func reset() -> void:
+	score = 0
+	deaths = 0
+
+
 func set_score(value: int) -> void:
 	score = value
 	emit_signal("score_updated")
