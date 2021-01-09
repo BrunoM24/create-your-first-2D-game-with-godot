bfc522849361251f63371b7d0fe50a02ca364d59
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-09 21:02:17 +0000

    UserInterface - Added the logic to show the updated Score && Appear on Player Death

--- src/ui/UserInterface.gd
+++ src/ui/UserInterface.gd
@@ -3,13 +3,31 @@ extends Control
 
 onready var scene_tree := get_tree()
 onready var pause_overlay : ColorRect = $PauseOverlay
+onready var score_label : Label = $Label
+onready var title_label : Label = $PauseOverlay/Title
 
 var paused := false setget set_paused
+
+
+func _ready() -> void:
+	PlayerData.connect("score_updated", self, "update_interface")
+	PlayerData.connect("player_died", self, "_on_PlayerData_player_died")
+	update_interface()
+
+
+func _on_PlayerData_player_died() -> void:
+	self.paused = true
+	title_label.text = "You died"
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		self.paused = not paused
+		scene_tree.set_input_as_handled()
+
+
+func update_interface() -> void:
+	score_label.text = "Score: %s" % PlayerData.score
 
 
 func set_paused(value: bool) -> void:
