25c45e13300687674b233c9f4b08190a0d24e9d4
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-11 21:34:27 +0000

    CoinCounter - Updated to use the new PlayerData CoinsCollected Info instead of the old Score

--- src/ui/CoinCounter.gd
+++ src/ui/CoinCounter.gd
@@ -5,8 +5,8 @@ onready var label : Label = $Label
 
 
 func _ready() -> void:
-	PlayerData.connect("score_updated", self, "_on_Coin_Collected")
+	PlayerData.connect("coin_collected", self, "_on_Coin_Collected")
 
 
 func _on_Coin_Collected() -> void:
-	label.text = str(PlayerData.score)
+	label.text = str(PlayerData.coins_collected)
