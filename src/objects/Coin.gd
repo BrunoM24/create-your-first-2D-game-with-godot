e0f128ac6cfd300dbc4cdc317421240c104fce04
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-12 21:44:01 +0000

    Coin - Removed the print()

--- src/objects/Coin.gd
+++ src/objects/Coin.gd
@@ -5,6 +5,5 @@ onready var animationPlayer : AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(body: Node) -> void:
-	print("Coin Collected")
 	PlayerData.coins_collected += 1
 	animationPlayer.play("fade_out")
